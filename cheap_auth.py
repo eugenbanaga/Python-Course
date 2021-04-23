8784d1ad7d48b019bd225fa4c0e4c22a8fda25f6
eugenbanaga <eugen.banaga@gmail.com>
2021-04-23 20:16:56 +0300

    Day 5 exercises

--- cheap_auth.py
+++ cheap_auth.py
@@ -3,21 +3,29 @@ from time import sleep
 username = "developer1"
 password = "hackme"
 
+auth_retries = 3
+
 print ("Description: This program authorize user in system\n")
 
-input_username = input("Please enter username: ")
-input_password = input("Please enter password: ")
+while auth_retries >= 0:
+    input_username = input("Please enter username: ")
+    input_password = input("Please enter password: ")
 
+    is_username_valid = username == input_username
+    is_password_valid = password == input_password
 
-is_username_valid = username == input_username
-is_password_valid = password == input_password
+    if is_username_valid and is_password_valid:
+        break
+    else:
+        auth_retries -= 1
+        if not is_username_valid:
+            print("Invalid username")
+        if not is_password_valid:
+            print("Invalid password")
+        if auth_retries == 0:
+            print("Auth retries exceeded")
+            exit(1)
 
-if not(is_username_valid and is_password_valid):
-    if not is_username_valid:
-        print("Invalid username")
-    if not is_password_valid:
-        print("Invalid password")
-    exit(1)
 
 print("Welcome " + input_username + ", you are authorized in to the system")
 shut_down = input("Do you want to shut down your computer yes/no:")
---
+++ errors.py
@@ -0,0 +1,15 @@
+print("This program will divide 2 numbers")
+
+stop_word = "stop"
+while True:
+    num1 = input("Enter first number: ")
+    num2 = input("Enter second number: ")
+
+    try:
+        num1 = float(num1)
+        num2 = float(num2)
+        print("Result: " + str(num1 / num2))
+    except ValueError as e:
+        print("Error:" + str(e))
+    except ZeroDivisionError as e:
+        print("Error: division by zero")
